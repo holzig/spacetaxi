88089565cbc1ee1dfda9e7b6006cb2c44a443040
cma <holzigg@gmail.com>
2020-01-31 17:20:00 +0100

    inital commit

---
+++ src/main/java/cma/otto/spacetaxi/App.java
@@ -0,0 +1,9 @@
+package cma.otto.spacetaxi;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("### RUNNING");
+    }
+
+}
---
+++ src/test/java/cma/otto/spacetaxi/SpikeTest.java
@@ -0,0 +1,14 @@
+package cma.otto.spacetaxi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SpikeTest {
+
+    @Test
+    public void testsAreExecuted() {
+        assertThat(true).isTrue();
+    }
+
+}
